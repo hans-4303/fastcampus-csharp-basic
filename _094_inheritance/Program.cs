9f8ca00eca3ac7aa1231affbdede4fdf825e45b5
hans <shinezl39@gmail.com>
2023-11-28 17:59:39 +0900

    상속 학습 중

---
+++ _094_inheritance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _094_inheritance
+{
+    public class Super
+    {
+        protected int a;
+        public void Print()
+        {
+            Console.WriteLine("Super Print()");
+        }
+    }
+
+    public class Sub: Super
+    {
+        private readonly int b = 1;
+        public new void Print()
+        {
+            Console.WriteLine("{0}, {1}", a, b);
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(/* string[] args */)
+        {
+            Sub sub = new Sub();
+            sub.Print();
+        }
+    }
+}
