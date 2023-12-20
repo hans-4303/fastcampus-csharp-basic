19114ae42aac7c2b4dc7ac24064ed68a7129a5dc
hans <shinezl39@gmail.com>
2023-12-20 13:49:39 +0900

    클래스에서 멤버 조작을 허용하는 인덱서: 클래스 자체에서 private 등으로 캡슐화된 멤버들을 제어할 수 있도록 우회할 방법 준 것에 가까움.

---
+++ _115_Indexer01/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _115_Indexer01
+{
+    /// <summary>
+    /// <para>인덱서</para>
+    /// <para>
+    ///     사실 인덱서라는 기능이 있다고 하기보다 클래스를 만들 때
+    ///     클래스 핵심 정보는 private 등을 통해 캡슐화할 거고 클래스 값을 직접 접근하도록 하지 않겠지만
+    ///     우회해서 값을 입력할 수 있도록 해주는 내용에 가깝다 보임.
+    /// </para>
+    /// <para>1. private를 적용한 멤버가 있다고 가정</para>
+    /// <para>2. this를 통해 인스턴스 자체가 호출됐을 때 취할 동작을 적음</para>
+    /// <para>
+    ///     접근지정은 public이기 때문에 클래스 밖에서도 사용 가능한 동작이 됨,
+    ///     반환 값은 int이기 때문에 동작하고 나서 반환 값이 int일 것을 뜻함,
+    ///     [int index]는 인스턴스를 호출할 때 aa[0]과 같이 동작할 수 있는 걸 뜻함,
+    ///     get 동작을 통해 멤버 배열의 요소에 접근할 수 있게 되고
+    ///     set 동작을 통해 할당되는 값을 멤버 배열 요소에 대입할 수 있게 됨.
+    /// </para>
+    /// </summary>
+    public class AA
+    {
+        private readonly int[] num = new int[10];
+
+        public int this[int index]
+        {
+            get { return num[index]; }
+            set { num[index] = value; }
+        }
+    }
+
+    internal class Program
+    {
+        /// <summary>
+        /// <para>인덱서를 통한 접근</para>
+        /// <para>1. 인스턴스를 생성함</para>
+        /// <para>2. 클래스에 지정된 동작대로 작동</para>
+        /// <para>
+        ///     aa[i] = i, aa[0] = 1000; 등은 this와 함께 지정된 set 동작,
+        ///     aa[i] 출력은 this와 함께 지정된 get 동작임.
+        /// </para>
+        /// </summary>
+        private static void Main (/* string[] args */)
+        {
+            AA aa = new AA();
+            for ( int i = 0; i < 10; i++ ) { aa[i] = i; }
+
+            aa[0] = 1000;
+            aa[1] = 100;
+
+            for ( int i = 0; i < 10; i++) Console.WriteLine("aa[{0}]: {1}", i, aa[i]);
+        }
+    }
+}
---
+++ _116_Indexer02/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _116_Indexer02
+{
+    /// <summary>
+    /// <para>Prop을 가지는 인덱서</para>
+    /// <para>1. Private 등으로 캡슐화된 멤버를 만듦</para>
+    /// <para>
+    ///     2. this[int index]를 통해 인스턴스가 호출됐을 때 동작 정의,
+    ///     index가 0보다 크거나 같고 arrayList.Count보다 작은지 따짐
+    ///     (arrayList.Count는 배열의 길이와 같음, 인덱스 아님)
+    /// </para>
+    /// <para>
+    ///     get 동작:
+    ///     멤버 ArrayList[index] 요소를 string 캐스팅해서 반환,
+    ///     조건 안 맞으면 null 반환
+    /// </para>
+    /// <para>
+    ///     set 동작:
+    ///     멤버 ArrayList[index] 요소에 value 값을 할당,
+    ///     만약 arrayList.Count와 같은 인덱스라면 새로 요소를 추가해서 작동함.
+    /// </para>
+    /// <para>3. 클래스에서 Prop을 정의</para>
+    /// <para>
+    ///     AA.Count:
+    ///     get 동작으로 현재 멤버 ArrayList의 Count를 반환함.
+    ///     어차피 ArrayList는 set 동작으로 변해야 하므로 AA.Count에 대한 Set 동작은 필요 없음.
+    /// </para>
+    /// </summary>
+    public class AA
+    {
+        private readonly ArrayList arrayList = new ArrayList();
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index < arrayList.Count)
+                    return (string) arrayList[index];
+                else
+                    return null;
+            }
+            set
+            {
+                if (index >= 0 && index < arrayList.Count)
+                    arrayList[index] = value;
+                else if (index == arrayList.Count)
+                    arrayList.Add(value);
+            }
+        }
+
+        public int Count
+        {
+            get { return arrayList.Count; }
+        }
+    }
+
+    /// <summary>
+    /// <para>Prop을 가지는 인덱서</para>
+    /// <para>1. 인스턴스 생성</para>
+    /// <para>2. 형식에 맞추어서 set 동작으로 값 대입, for문을 통한 반복 || 인덱스를 통한 할당</para>
+    /// <para>3. get 동작으로 값 호출, 호출 시 string 타입으로 캐스팅</para>
+    /// </summary>
+    internal class Program
+    {
+        private static void Main ()
+        {
+            AA aa = new AA();
+            for (int i = 0; i < 10; i++)
+            {
+                // aa[i] = i;
+                aa[i] = string.Format("{0}", i);
+            }
+
+            aa[0] = "Hello";
+            aa[1] = "World";
+
+            for (int i = 0; i < aa.Count; i++)
+            {
+                Console.WriteLine("aa: " + aa[i]);
+            }
+        }
+    }
+}
